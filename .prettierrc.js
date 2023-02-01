220e522580bc13044428e49a82e93f362f0a1eb2
edxv <ed@incred.co.uk>
2023-02-01 11:17:09 +0000

    fix tsconfig

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  endOfLine: "lf",
+  trailingComma: "none",
+  plugins: [require.resolve("prettier-plugin-organize-imports")],
+};
